fae19d9e6d032aa2f52280a6f90a9f6b6187df41
ratseno <onestar1014@gmail.com>
2020-05-21 22:14:15 +0900

    연습

---
+++ src/example/codility/BinaryGap.java
@@ -0,0 +1,27 @@
+package example.codility;
+
+public class BinaryGap {
+    public static int solution(int N) {
+        // write your code in Java SE 8
+        String binaryString = Integer.toBinaryString(N);
+
+        char[] binaryChar = binaryString.toCharArray();
+        int totalGapLength = 0;
+        int gapLength = 0;
+
+        for(int i=0;i<binaryChar.length;++i){
+            if(binaryChar[i] == '1'){
+                if(gapLength > totalGapLength) totalGapLength = gapLength;
+                gapLength = 0;
+            }else{
+                ++gapLength;
+            }
+        }
+
+        return totalGapLength;
+    }
+
+    public static void main(String[] args) {
+        solution(529 );
+    }
+}
