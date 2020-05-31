a3e3391fe06c432dd88cff262038f016f1df47c0
ratseno <onestar1014@gmail.com>
2020-05-31 20:18:27 +0900

    연습

---
+++ src/example/codility/PermCheck.java
@@ -0,0 +1,28 @@
+package example.codility;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PermCheck {
+
+    public static int solution(int[] A){
+        Set<Integer> marks = new HashSet<>();
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > A.length) {
+                return 0;
+            }
+
+            if (marks.contains(A[i])) {
+                return 0;
+            }
+
+            marks.add(A[i]);
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int res = solution(new int[]{1,4,2});
+    }
+}
