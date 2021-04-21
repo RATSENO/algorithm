4f69eca352a9eb1f6e0e3e58f05b00311e5c6faf
ratseno <onestar1014@gmail.com>
2021-04-21 21:56:32 +0900

    leetcode p191

--- src/example/leetcode/P191.java
+++ src/example/leetcode/P191.java
@@ -6,7 +6,8 @@ public class P191 {
     private static int hammingWeight(int n) {
         int cnt = 0;
         while(n!=0) {
-            //비트연산자 계산해서 1이 나오면 (n하나씩 씨프트 해서 계산했을 때)1인지 확인 가능
+            //비트연산자 계산해서 1이 나오면
+            // (n하나씩 씨프트 해서 계산했을 때)1인지 확인 가능
             cnt = cnt + (n & 0b00000000000000000000000000000001);
             n = n>>>1;
         }
