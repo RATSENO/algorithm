d22b74fe350ae0b514d78bbf9def419c153d7415
ratseno <onestar1014@gmail.com>
2021-04-21 21:57:25 +0900

    leetcode p191

--- src/example/leetcode/P191.java
+++ src/example/leetcode/P191.java
@@ -5,6 +5,7 @@ public class P191 {
     // you need to treat n as an unsigned value
     private static int hammingWeight(int n) {
         int cnt = 0;
+
         while(n!=0) {
             //비트연산자 계산해서 1이 나오면
             // (n하나씩 씨프트 해서 계산했을 때)1인지 확인 가능
